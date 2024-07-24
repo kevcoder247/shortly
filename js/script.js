e6b9ac8805e98cf203c7385344b76d26755b88bc
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2024-07-24 00:10:13 -0400

    wip: add url validation

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const input = document.getElementById('link-input');
+const form = document.getElementById('link-form');
+const errMsg = document.getElementById('err-msg');
+
+form.addEventListener('submit', formSubmit);
+
+
+// Validate a URL
+function validURL(str) {
+  let pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  )
+  return !!pattern.test(str)
+}
+
+function formSubmit(e) {
+  e.preventDefault()
+
+  if (input.value === '') {
+    errMsg.innerHTML = 'Please enter something'
+    input.classList.add('border-red')
+  } else if (!validURL(input.value)) {
+    errMsg.innerHTML = 'Please enter a valid URL'
+    input.classList.add('border-red')
+  } else {
+    errMsg.innerHTML = ''
+    input.classList.remove('border-red')
+    alert('Success')
+  }
+}
